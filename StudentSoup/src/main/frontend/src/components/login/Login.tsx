640e3206dd31917cb610e76a5db07744e631d986
KanghoYoo <kangsick98@gmail.com>
2023-02-17 18:18:38 +0900

    - feat : login 페이지 아이디저장, 로그인 요청기능, 페이지 이동 구현

--- StudentSoup/src/main/frontend/src/components/login/Login.tsx
+++ StudentSoup/src/main/frontend/src/components/login/Login.tsx
@@ -1,29 +1,80 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import MainNavbar from '../common/mainNavbar';
 import './login.scss';
+import useInput from '../../hooks/useInput';
+import { signIn } from '../../apis/auth/AuthAPI';
 
 const Login = () => {
+  const [userId, onChangeUserId, setUserId] = useInput('');
+  const [userPassword, onChangeUserPassword] = useInput('');
+  const [rememberId, setRememberId] = useState('unchecked-remember-id');
+
+  const navigate = useNavigate();
+
+  const onClickRememberId = () => {
+    if (rememberId === 'unchecked-remember-id') {
+      setRememberId('checked-remember-id');
+      localStorage.setItem('rememberId', userId);
+    } else if (rememberId === 'checked-remember-id') {
+      setRememberId('unchecked-remember-id');
+      localStorage.removeItem('rememberId');
+    }
+  };
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>): void => {
+      e.preventDefault();
+      onClickRememberId();
+      signIn(userId, userPassword).then(response => {
+        console.log(response);
+        // TODO: 로그인 정보 저장 코드 및 에러처리 구현하기
+        navigate('/');
+      });
+    },
+
+    [userId, userPassword],
+  );
+
+  useEffect(() => {
+    const rememberIdValue: string | null = localStorage.getItem('rememberId');
+    if (rememberIdValue != null) {
+      setRememberId('checked-remember-id');
+      setUserId(localStorage.getItem('rememberId'));
+    }
+  }, []);
   return (
     <>
       <div className="background">
         <MainNavbar />
         <div className="main">
           <h2>로그인</h2>
-          <form>
-            <input type="text" className="id" placeholder="아이디 또는 이메일을 입력해주세요" />
-            <input type="password" className="password" placeholder="비밀번호를 입력해주세요" />
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              className="id"
+              value={userId}
+              onChange={onChangeUserId}
+              placeholder="아이디 또는 이메일을 입력해주세요"
+            />
+            <input
+              type="password"
+              className="password"
+              value={userPassword}
+              onChange={onChangeUserPassword}
+              placeholder="비밀번호를 입력해주세요"
+            />
             <div className="login-keep-wrap">
-              <div className="remember-wrap">
-                <div className="unchecked-remember-id" />
+              <div className="remember-wrap" onClick={onClickRememberId}>
+                <div className={rememberId} />
                 <span>아이디 저장</span>
               </div>
-              <Link to="/">아이디/비밀번호 찾기</Link>
+              <Link to="/find">아이디/비밀번호 찾기</Link>
             </div>
             <button className="login-button" type="submit">
               로그인
             </button>
-            <Link to="/" className="signup-link">
+            <Link to="/signup" className="signup-link">
               <button className="signup-button">회원가입</button>
             </Link>
           </form>
